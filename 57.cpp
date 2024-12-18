fca122e78a548ce4aa6af82c5dc6c6efb45830d4
Charlotte Roy <roychrltt@chacha.home>
2024-12-18 11:13:00 +0100

    18-12-2024 matin

--- 57.cpp
+++ 57.cpp
@@ -4,7 +4,16 @@
 #include <algorithm>
 #include <string>
 
-
+std::vector<std::vector<int>> f(std::vector<std::vector<int>>& intervals, std::vector<int>& newInterval) {
+	auto beg = intervals.begin(), end = intervals.end();
+	int& a = newInterval[0], & b = newInterval[1];
+	auto l = lower_bound(beg, end, a, [](auto& val, int b) { return val[1] < b; });
+	auto r = upper_bound(  l, end, b, [](int b, auto& val) { return val[0] > b; });
+	if (l != end) a = std::min(a, l[ 0][0]);
+	if (r != beg) b = std::max(b, r[-1][1]);
+	intervals.insert(intervals.erase(l, r), newInterval);
+	return intervals;
+}
 
 int main()
 {
@@ -13,5 +22,5 @@ int main()
 	std::cout << ans << std::endl;
 	return 0;
 }
-// runtime beats %
-// memory beats %
+// runtime beats 100%
+// memory beats 20.20%
