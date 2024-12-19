553da215e85c1f0bd70806ae6dda21698d26bf50
Charlotte Roy <roychrltt@chacha.home>
2024-12-19 21:16:44 +0100

    19-12-2024 soir

--- 769.cpp
+++ 769.cpp
@@ -3,26 +3,28 @@
 #include <unordered_map>
 #include <algorithm>
 #include <string>
+#include <numeric>
 
 int f(std::vector<int>& arr)
 {
-	int ans = 1, x = arr[0], n = arr.size();
-	for (int i = 0; i < n; i++) {
-		if (arr[i] > x && arr[i] <= i) {
-			x = arr[i];
+	int n = arr.size();
+	std::vector<int> count(n);
+	int ans = 0;
+	for (int i = 0; i < n; i++)
+	{
+		count[arr[i]] = 1;
+		if (std::accumulate(count.begin(), count.begin() + i + 1, 0) == i + 1)
 			ans++;
-		}
-		std::cout << "x: " << x << std::endl;
 	}
 	return ans;
 }
 
 int main()
 {
-	std::vector<int> v = {0,3,1,2};
+	std::vector<int> v = {3,2,1,0};
 	int ans = f(v);
 	std::cout << ans << std::endl;
 	return 0;
 }
-// runtime beats %
-// memory beats %
+// runtime beats 100%
+// memory beats 7.22%
