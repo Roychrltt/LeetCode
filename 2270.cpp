ce39b9d7d88fa44a61dac9738aa7b8f97285b2f6
Charlotte <roychrltt@gmail.com>
2025-01-03 10:46:27 +0100

    03-01-2025, I have many to say so I'll say it all here. First, I realised that it's my very self who has done the most hurt to me.I always have too many worries and fears and I always doubt, even believe, and not just expect, the worst to happen. It has ruined my mental health and I absolutely have no idea how to cope with it. Second, so you're telling me that all the good times and good people are just a mirage? That I should just forget all and let go? I don't think I'll be able to do it... alone

---
+++ 2270.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+
+int waysToSplitArray(std::vector<int>& nums) {
+	//long long total = std::reduce(nums.begin(), nums.end());
+	long long total = 0;
+	for (long long n : nums)
+		total += n;
+	long long sum = 0;
+	int count = 0;
+	for (int i = 0; i < nums.size() - 1; i++)
+	{
+		sum += nums[i];
+		if (total - sum <= sum) count++;
+	}
+	return count;
+}
+
+int main()
+{
+	std::vector<int> v = {};
+	int ans = f(v);
+	std::cout << ans << std::endl;
+	return 0;
+}
+// runtime beats 100%
+// memory beats 62.32%
