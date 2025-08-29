3aea6c73f7138620a74e45498da1077b36b0c270
Charlotte <roychrltt@gmail.com>
2025-08-30 00:23:31 +0200

    .

---
+++ 3021.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+long long flowerGame(int n, int m) {
+	return (long long)m * n / 2;
+}
+
+int main()
+{
+	std::vector<int> v = {};
+	int ans = f(v);
+	std::cout << ans << std::endl;
+	return 0;
+}
+// runtime beats %
+// memory beats %
